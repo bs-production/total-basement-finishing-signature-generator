36ad65b04534210e747d4666ff4db93165eeb480
chrisroselli <chrisroselli@gmail.com>
2020-04-28 09:18:06 -0400

    update

--- src/components/output-signature.vue
+++ src/components/output-signature.vue
@@ -109,7 +109,7 @@
             </td>
           </tr>
         </table>
-        <p><strong>Discover all of our service: </strong><a href="http://www.ctbasementcompanies.com/">ctbasementcompanies.com</a></p>
+        <p><strong>Discover all of our services: </strong><a href="http://www.ctbasementcompanies.com/">ctbasementcompanies.com</a></p>
           <p style="font-size: 10px; color: #000; line-height: 12px;text-align: left; width: 450px" width="450">This message contains confidential information and is intended only for the intended recipients. If you are not an intended recipient you should not disseminate, distribute or copy this e-mail. Please notify us immediately by e-mail if you have received this e-mail by mistake and delete this e-mail from your system. E-mail transmission cannot be guaranteed to be secure or error-free as information could be intercepted, corrupted, lost, destroyed, arrive late or incomplete, or contain viruses. Therefore we do not accept liability for any errors or omissions in the contents of this message, which arise as a result of e-mail transmission. If verification is required please request a hard-copy version.</p>
     </section>
   </div>
